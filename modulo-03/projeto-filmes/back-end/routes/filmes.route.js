7b59cbd3579466a77625e073ce4380815de43dc3
Gustavo Fogia <gustavofogia@yahoo.com.br>
2021-12-13 19:57:15 -0300

    New Commit

---
+++ modulo-03/projeto-filmes/back-end/routes/filmes.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+const filmesController = require('../controllers/filmes.controllers');
+
+router.get('/', filmesController.getFilme)
+
+router.get('/:id', filmesController.getFilmesById)
+
+router.post('/add', filmesController.postFilme)
+
+router.put('/edit/:id', filmesController.putFilme)
+
+router.delete('/delete/:id', filmesController.deleteFilme)
+
+module.exports = router;
