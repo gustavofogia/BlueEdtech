4b856ec6f7438eeff48a10838f0340e96a1ec846
Gustavo Fogia <gustavofogia@yahoo.com.br>
2021-12-13 00:04:40 -0300

    New Commit

--- modulo-03/projeto-filmes/back-end/controllers/filmes.controllers.js
+++ modulo-03/projeto-filmes/back-end/controllers/filmes.controllers.js
@@ -29,7 +29,29 @@ const putFilme = (req, res) => {
     if(!req.body || !req.body.nome || !req.body.img || !req.body.genero || !req.body.nota) {
         res.status(400).send({message: 'Não foi possivel editar, por favor preencha todos os campos.'})
     }
-    
+    const idParam = req.params.id
+    const filmeEdit = req.body
+    const edicao = filmeService.putFilme(idParam, filmeEdit);
+    if(edicao) {
+        res.send({message: `O filme foi editado com sucesso!`})
+    } else {
+        res.status(404).send({message: `Não encontramos o filme para editar`})
+    }
 }
 
+const deleteFilme = (req, res) => {
+    const id = req.params.id;
+    const filmeExcluido = filmesService.deleteFilme(req.params.id);
+    if(!filmeExcluido) {
+        res.status(404).send({message: 'Nao foi possivel excluir, o id nao foi encontrado'})
+    }
+    res.send({message: `O filme ${filmeExcluido.nome} do genero ${filmeExcluido.genero} foi excluido com sucesso`});
+}
 
+module.exports = {
+    getFilme,
+    getFilmesById,
+    postFilme,
+    putFilme,
+    deleteFilme
+}
