31311c1e932326dc9a7abb9c74ccfde3d3073193
Gustavo Fogia <gustavofogia@yahoo.com.br>
2021-12-29 15:29:38 -0300

    New Commit

--- modulo-03/aula-08/back-end/routes/games.routes.js
+++ modulo-03/aula-08/back-end/routes/games.routes.js
@@ -1,10 +1,25 @@
-//importar o express
+// importar o express
 const express = require('express');
+const gamesController = require('./../controllers/games.controller')
 
-//inicializar p modulo de rotas
+// inicializar o modulo de rotas
 const router = express.Router();
 
+//[ENDPOINTS]
+// [GET] /games - Retorna uma lista de jogos pré cadastrados no banco
+router.get('/', gamesController.getGames);
 
+//[GET] /games/{id} - retorna um game de acordo com o seu id
+router.get('/:id', gamesController.getGameById);
 
-//expotamos o modulo de rotas
+// [POST] /games/add - Cadastra um novo jogo no banco de dados
+router.post('/add', gamesController.createGame);
+
+// [PUT] /games/edit/{id} - Edita os dados do game de acordo com o id e o objeto recebido
+router.put('/edit/:id', gamesController.editGame);
+
+// [DELETE] /games/delete - Exclui um game do banco de dados de acordo com o seu id
+router.delete('/delete/:id', gamesController.deleteGame);
+
+//exportamos o modulo de rotas
 module.exports = router;
